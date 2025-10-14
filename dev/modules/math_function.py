34143c9e7e474c3f8efede98072efa4777352f95
Huss Ala <husseinlego@gmail.com>
2025-10-14 17:43:40 -0400

    <span style=" font-weight:700;">Fait le sqeulette des fonctions mathématiques</span> <span style=" color:#90caf9;">(MathFunction, Expo et b^x, Trig et sin (modifié un peu celui de wassim), poly et quad -&gt; </span><span style=" color:#fbbc9f;">Poly fonctionne pour n'importe quel degré</span><span style=" color:#90caf9;"> et </span><span style=" color:#dcc9f6;">expo fonctionne avec exposants et logs</span><span style=" color:#90caf9;">), </span><span style=" font-weight:700;">Fait la base base base de IntParameters </span>

--- dev/modules/math_function.py
+++ dev/modules/math_function.py
@@ -1,7 +1,7 @@
 # imports
 from abc import ABC,abstractmethod
 from typing import override, TypeAlias, List
-from real_parameters import RealParameter
+from real_parameters import RealParameter, IntParameter
 import numpy as np
 
 
@@ -77,10 +77,101 @@ class SinFunction(TrigFunction):
 #        super.__init__("Cos function","my details","a description of cos function")
 
 
+
+class BaseExpoFunction(MathFunction, ABC):
+    param_specs = [ 
+        # static data, should be on a different file for organization purposes
+        ("a","Coefficient","Facteur d'échelle vertical", -5.0, 5.0, 1.0),
+        ("b","Base","Base de l'exposant", -5.0, 5.0, 1.0),
+        ("c","c","Décalage vertical",-5.0, 5.0, 1.0),
+    ]
+
+    param_order = ['a','b','c'] # defines order of the parameters (a is first, f second..)
+    # form : f(x) = a * b**x + c
+
+    def __init__(self, name, details, description):
+        super().__init__(name, details, description)
+
+        self._parameters = [RealParameter(*x) for x in self.param_specs]
+
+    @override
+    def get_param(self,param_symbol)-> RealParameter:
+        return self._parameters[self.param_order.index(param_symbol)]
+    
+    @abstractmethod
+    def process(self,x: number) -> number:
+        return
+    
+
+class ExpoFunction(BaseExpoFunction):
+    def __init__(self):
+        super().__init__("Exponential", "details", "description")
+
+    @override
+    def process(self, x: number) -> number:
+        a = self.get_param('a').value
+        b = self.get_param('b').value
+        c = self.get_param('c').value
+        return a * b**x + c
+
+
+class PolyFunction(MathFunction, ABC):
+    param_specs = [ 
+        # static data, should be on a different file for organization purposes
+        ("n","Degré","Degré de la fonction", 0, 5, 2),
+        ("a","Coefficients","Coefficient des variables", -5.0, 5.0, 1.0)
+    ]
+    
+    def __init__(self, name, details, description):
+        super().__init__(name, details, description)
+
+        self._n = IntParameter(*self.param_specs[0]) 
+
+        self._a: list[RealParameter] = []
+        self._update_coeffs()
+
+    def _update_coeffs(self):
+        degree = self._n.value
+
+        self._a = []
+        for i in range(degree, -1, -1):
+            coeff_specs = (f"a{i}", f"Coefficient a{i}", f"Coefficient de x^{i}", -5.0, 5.0, 1.0)
+            param = RealParameter(*coeff_specs)
+            self._a.append(param)
+
+    @override
+    def get_param(self, param_symbol: str) -> RealParameter:
+        if param_symbol == "n":
+            return self._n
+        else:
+            for param in self._a:
+                if param.symbol == param_symbol:
+                    return param
+        
+    @override
+    def process(self, x: number) -> number:
+        coeffs = [param.value for param in self._a]
+        poly = np.poly1d(coeffs)
+        return poly(x)
+    
+
+class QuadFunction(PolyFunction):
+    def __init__(self):
+        super().__init__("Quadratic Funtion", "details", "the description of a quad function is ..")
+
 # usage exemple
 f = SinFunction()
+
+
 print(f.description) # get a description of a function (sin)
 
 print(f.get_param('a').description) # get a description of a specified parameter
 
 print(f.process(np.pi/2)) # process a value
+
+
+e = ExpoFunction()
+print(e.process(2))
+
+quad = QuadFunction()
+print(quad.process(10))
